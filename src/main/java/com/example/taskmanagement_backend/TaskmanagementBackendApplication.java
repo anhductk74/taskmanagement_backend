20626c4158b1182b0b89cf99d9b1566e4a4a9032
anhductk74 <anhduc120102@gmail.com>
2025-07-28 09:08:02 +0700

    first commit

---
+++ src/main/java/com/example/taskmanagement_backend/TaskmanagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagement_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagementBackendApplication.class, args);
+	}
+
+}
