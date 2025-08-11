a3946f4c8ab5f59ef7aa9e98a3cd5ce1cf8fc16e
anhductk74 <anhduc120102@gmail.com>
2025-08-11 10:14:39 +0700

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/com/example/taskmanagement_backend/dtos/ProjectInvitatinDto/ProjectInvitationResponseDto.java
@@ -0,0 +1,21 @@
+package com.example.taskmanagement_backend.dtos.ProjectInvitatinDto;
+
+import com.example.taskmanagement_backend.enums.InvitationStatus;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ProjectInvitationResponseDto {
+    private Long id;
+    private String email;
+    private Long projectId;
+    private String projectName;
+    private Long invitedById;
+    private String invitedByName;
+    private InvitationStatus status;
+    private String token;
+    private LocalDateTime createdAt;
+}
